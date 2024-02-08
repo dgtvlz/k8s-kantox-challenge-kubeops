27709546594d01930db1bc62da2a535b421ee480
Diego Garcia <diegogarcia@netrixllc.com>
2024-02-08 15:49:26 -0300

    initial commit

---
+++ src/hello-world/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+const process = require('process');
+const { v4: uuidv4 } = require('uuid');
+
+var expressapp = express();
+const uniqueId = uuidv4();
+
+expressapp.get('/', function (req, res) {
+    const message = {
+        message: 'Hello World JavaScript v1',
+        id: uniqueId
+    };
+    res.json(message);
+});
+
+expressapp.listen(5000, function () {
+    console.log('Ready on port 5000!');
+});
+
+/* To Handle Ctrl C */
+process.on('SIGINT', function () {
+    process.exit();
+});
