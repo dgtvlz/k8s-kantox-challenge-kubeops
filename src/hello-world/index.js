ac322ba6f6a29a113266913937213dc86b780fe9
Diego Garcia <diegogarcia@netrixllc.com>
2024-02-09 13:54:04 -0300

    updating docker

--- src/hello-world/index.js
+++ src/hello-world/index.js
@@ -7,7 +7,7 @@ const uniqueId = uuidv4();
 
 expressapp.get('/', function (req, res) {
     const message = {
-        message: 'Hello World JavaScript v2.4',
+        message: 'Hello World JavaScript v2.5',
         id: uniqueId
     };
     res.json(message);
